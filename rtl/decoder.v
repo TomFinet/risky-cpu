acffacad44c97eb8da0091272ea9ac55c322cafc
Tom Finet <tom.codeninja@gmail.com>
2022-09-14 18:15:05 +0200

    finished decoder for non-pipelined cpu

--- rtl/decoder.v
+++ rtl/decoder.v
@@ -1,92 +1,174 @@
-// we have 7 bits for opcode
-// our opcodes for the base integer instruction set.
-`define OP_IMM   7'b0000000
-`define OP       7'b0000001
-`define LUI      7'b0000010
-`define AUIPC    7'b0000011
-`define JAL      7'b0000100
-`define JALR     7'b0000101
-`define BRANCH   7'b0000110
-`define LOAD     7'b0000111
-`define STORE    7'b0001000
-`define MISC_MEM 7'b0001001
-`define SYSTEM   7'b0001010
-
+`include "codes.v"
 
 module decoder (
 	input clock,
 	input enable,
 
-	input [31:0] instruction,
+	input [31:0] inst,
 
-	output reg alu_op,
     output reg [3:0] func,
 	output reg [11:0] imm,
 	output reg [4:0] rd, rs, rs2
 
-    output reg reg_we; // register bank write enable
+    output reg [3:0] store_func,
+    output reg [3:0] load_func,
+
+    /* control signals */
+    output reg reg_we,
+    output reg reg_sel,
+    output reg lui_sel,
+    output reg pc_sel,
+    output reg b_sel
 );
 
 	always @(posedge clock && enable) begin
 		
-        case (instruction[6:0])
+        case (inst[6:0])
 
             OP_IMM:
-                rd     <= instruction[11:7];
-                func   <= {instruction[30], instruction[14:12]};
-                rs     <= instruction[19:15]; 
-                imm    <= {{20{instruction[31]}}, instruction[31:20]};
-                reg_re <= 1;
+                rd      <= inst[11:7];
+                rs      <= inst[19:15]; 
+                func    <= {inst[30], inst[14:12]};
+
+                if (inst[14:12] === SRA[3:1]) begin
+                    imm <= {{27{1'b0}}, inst[24:20]}
+                end
+                else begin
+                    imm <= {{20{inst[31]}}, inst[31:20]};
+                end
+                
+                // control signals
+                reg_we  <= REG_WRITE;
+                reg_sel <= REG_RES;
+                b_sel   <= B_IMM;
+                lui_sel <= LUI_0;
+                pc_sel  <= PC_PLUS_4;
+                mem_enable <= MEM_NOT_ENABLE;
 
             OP:
-                rd     <= instruction[11:7];
-                func   <= {instruction[30], instruction[14:12]};
-                rs     <= instruction[19:15];
-                rs2    <= instruction[24:20];
-                imm    <= {{20{instruction[31]}}, instruction[31:20]};
-                reg_re <= 1;
+                rd      <= inst[11:7];
+                func    <= {inst[30], inst[14:12]};
+                rs      <= inst[19:15];
+                rs2     <= inst[24:20];
+                imm     <= {{20{inst[31]}}, inst[31:20]};
+                
+                reg_we  <= REG_WRITE;
+                reg_sel <= REG_RES;
+                b_sel   <= B_REG;
+                lui_sel <= LUI_0;
+                pc_sel  <= PC_PLUS_4;
+                mem_enable <= MEM_NOT_ENABLE;
 
             LUI:
-                rd     <= instruction[11:7];
-                imm    <= {instruction[31:12], {12{0}}};
+                rd      <= inst[11:7];
+                imm     <= {inst[31:12], {12{0}}};
+                
+                reg_we  <= REG_WRITE;
+                reg_sel <= REG_IMM;
+                lui_sel <= LUI_0;
+                pc_sel  <= PC_PLUS_4;
+                mem_enable <= MEM_NOT_ENABLE;
              
             AUIPC:
-                rd     <= instruction[11:7];
-                imm    <= {instruction[31:12], {12{0}}};
+                rd      <= inst[11:7];
+                imm     <= {inst[31:12], {12{0}}};
+                
+                reg_we  <= REG_WRITE;
+                reg_sel <= REG_IMM;
+                lui_sel <= LUI_PC;
+                pc_sel  <= PC_PLUS_4;
+                mem_enable <= MEM_NOT_ENABLE;
 
             JAL:
-                rd     <= instruction[11:7];
-                imm    <= instruction[31:12];
+                rd      <= inst[11:7];
+                imm     <= {
+                                {12{inst[31]}},
+                                inst[19:12],
+                                inst[20],
+                                inst[30:21],
+                                1'b0
+                            };
+                
+                reg_we  <= REG_WRITE;
+                reg_sel <= REG_PC_PLUS_4;
+                lui_sel <= LUI_PC;
+                pc_sel  <= PC_PLUS_IMM;
+                mem_enable <= MEM_NOT_ENABLE;
 
             JALR:
-                rd     <= instruction[11:7];
+                rd      <= inst[11:7];
+                rs      <= ins[19:15];
+                imm     <= {{20{inst[31]}}, inst[31:20]};
 
+                func    <= ADD;
+                
+                b_sel   <= B_IMM;
+                reg_we  <= REG_WRITE;
+                reg_sel <= REG_PC_PLUS_4;
+                pc_sel  <= PC_REG_PLUS_IMM;
+                lui_sel <= LUI_0;
+                mem_enable <= MEM_NOT_ENABLE;
 
             BRANCH:
-                rs     <= instruction[19:15];
-                rs2    <= instruction[24:20];
-                func   <= instruction[14:12];
-                imm    <= // offsets
+                rs       <= inst[19:15];
+                rs2      <= inst[24:20];
+                
+                if (
+                    inst[14:12] === BLT[3:1] || 
+                    inst[14:12] === BLTU[3:1]
+                ) begin
+                    func <= {1'b0, inst[14:12]};
+                end
+                else begin
+                    func <= {1'b1, inst[14:12]}
+                end
+                imm      <= { // offset
+                                {20{inst[31]}},
+                                inst[7],
+                                inst[30:25],
+                                inst[11:8],
+                                1'b0
+                            }
+                
+                reg_we   <= REG_NO_WRITE;
+                b_sel    <= B_REG;
+                lui_sel  <= LUI_PC;
+                pc_sel   <= PC_BRANCH;
+                mem_enable <= MEM_NOT_ENABLE;
 
             LOAD:
-                rd     <= instruction[11:7];
-                func   <= instruction[14:12];
-                rs     <= instruction[19:15];
-                imm    <= instruction[31:20];
-
+                rd      <= inst[11:7];
+                load_func <= inst[14:12];
+                func    <= ADD;
+                rs      <= inst[19:15];
+                imm     <= {{20{inst[31]}}, inst[31:20]};
+                
+                reg_we  <= REG_WRITE;
+                reg_sel <= REG_RAM;
+                b_sel   <= B_IMM;
+                lui_sel <= LUI_0;
+                pc_sel  <= PC_PLUS_4;
+                mem_rw  <= MEM_READ;
+                mem_enable <= MEM_ENABLE;
+                
             STORE:
-                func   <= instruction[14:12];
-                rs     <= instruction[19:15];
-                rs2    <= instruction[24:20];
-                imm    <= instruction[31:25];
+                func   <= ADD;
+                store_func <= inst[14:12];
+                rs     <= inst[19:15];
+                rs2    <= inst[24:20];
+                imm    <= {
+                            {20{inst[31]}},
+                            inst[31:25],
+                            inst[11:8],
+                            inst[7]
+                         };
 
-            MISC_MEM:
-
-
-            SYSTEM:
-
-
-
+                b_sel   <= B_IMM;
+                lui_sel <= LUI_0;
+                reg_we  <= REG_NO_WRITE;
+                pc_sel  <= PC_PLUS_4;
+                mem_rw  <= MEM_WRITE;
+                mem_enable <= MEM_ENABLE;
         endcase
 	end
 
