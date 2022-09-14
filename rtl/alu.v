1cd73724881404e0d99c872472718c45f9f7c8f8
Tom Finet <tom.codeninja@gmail.com>
2022-09-14 18:13:29 +0200

    added signed comparison

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,14 +1,4 @@
-/* Reg -- Reg instructions */
-`define ADD  4'b0000
-`define SLT  4'b0001
-`define SLTU 4'b0010
-`define AND  4'b0011
-`define OR   4'b0100
-`define XOR  4'b0101
-`define SLL  4'b0110
-`define SRL  4'b0111
-`define SUB  4'b1000
-`define SRA  4'b1001
+`include "codes.v"
 
 module alu (
     input clock,
@@ -18,14 +8,15 @@ module alu (
 
     input [31:0] a, b,
 
-	output [31:0] res 
+	output [31:0] res
 );
 
     always @(posedge clock && enable) begin
         
         case (func)
             ADD     : res <= a + b;
-            SLT     : res <= a < b;
+            LT      : res <= $signed(a) < $signed(b);
+            LTU     : res <= a < b;
             AND     : res <= a & b;
             OR      : res <= a | b;
             XOR     : res <= a ^ b;
@@ -33,6 +24,12 @@ module alu (
             SRL     : res <= a >> b;
             SUB     : res <= a - b;
             SRA     : res <= a >>> b;
+            
+            EQ      : res <= a === b;
+            NE      : res <= a !== b;
+            LE      : res <= $signed(a) >= $signed(b);
+            LEU     : res <= a >= b;
+
             default : res <= 0;
         endcase
 
