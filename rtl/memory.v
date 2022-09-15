ae18b61b8771422b898e072c7f5336b9177a20e2
Tom Finet <tom.codeninja@gmail.com>
2022-09-15 11:54:00 +0200

    only one always block

--- rtl/memory.v
+++ rtl/memory.v
@@ -1,13 +1,13 @@
-module memory(
-	
+module memory (
     input clock,
-	input enable,
 	input reset,
+    
+    input [31:0] inst_ain, // for instruction reading
+    output reg [31:0] inst_dout,
 
 	input rw,
 	input [31:0] ain,
 	input [31:0] din,
-
 	output reg [31:0] dout
 );
 
@@ -16,20 +16,19 @@ reg [31:0] mem[1023:0];
 
 integer i;
 
-always @(posedge reset) begin
-	if(reset) begin
-    for (i = 0; i < 1024; i = i + 1)
-        mem[i] = 0;
-    end
-end
-
 always @(posedge clock) begin
-    if(enable) begin
-        if(rw) begin // write
-            mem[ain] = din;
+    if(reset) begin
+        for (i = 0; i < 1024; i = i + 1)
+            mem[i] <= 0;
+    end
+    else begin
+        inst_dout <= mem[inst_ain];
+
+        if(rw) begin
+            mem[ain] <= din;
         end
-        else begin // read
-            dout = mem[ain];
+        else begin
+            dout <= mem[ain];
         end
     end
 end
