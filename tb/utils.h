316101f653d25a8382014db8bd5b5e50a4534b05
Tom Finet <tom.codeninja@gmail.com>
2022-09-18 10:52:26 +0200

    added int to bstring function

---
+++ tb/utils.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define b(x) int_to_binary(x)
+
+const char* int_to_binary(uint x) {
+    static char b[sizeof(x)*8] = {0};
+    int y;
+    long long z;
+
+    for (z = 1LL<<sizeof(int)*8-1, y = 0; z > 0; z >>= 1, y++) {
+        b[y] = (((x & z) == z) ? '1' : '0');
+    }
+    b[y] = 0;
+
+    return b;
+}
