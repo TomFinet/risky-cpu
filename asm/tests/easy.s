823ecdd09c28caf3238ee09609c3e2dd2c554e1f
Tom Finet <tom.codeninja@gmail.com>
2022-09-18 15:08:55 +0200

    renamed

--- asm/tests/easy.s
+++
@@ -1,5 +0,0 @@
-addi x1, zero, 1
-addi x2, zero, 2
-addi x3, zero, 3
-addi x4, zero, 4
-addi x5, zero, 5
--- asm/tests/fib.s
+++
@@ -1,6 +0,0 @@
-addi x1, zero, 1
-addi x2, zero, 1
-
-mov x3, x2
-add x2, x2, x1
-mov x1, x3
