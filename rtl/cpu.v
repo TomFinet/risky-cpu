1b0f03fbdee2a9b5e0a32edea26300fae2c6552c
Tom Finet <tom.codeninja@gmail.com>
2022-09-18 10:51:26 +0200

    fixed pipeline and converted reg -> wire

--- rtl/cpu.v
+++ rtl/cpu.v
@@ -2,89 +2,85 @@
 `define CPU
 
 `include "./rtl/codes.v"
-`include "./rtl/if/pc.v"
+`include "./rtl/pc.v"
 `include "./rtl/register_bank.v"
 
-`include "./rtl/rr/rr_decoder.v"
-`include "./rtl/alu/alu_decoder.v"
-`include "./rtl/mem/mem_decoder.v"
-`include "./rtl/wb/wb_decoder.v"
+`include "./rtl/rr_decoder.v"
+`include "./rtl/alu_decoder.v"
+`include "./rtl/mem_decoder.v"
+`include "./rtl/wb_decoder.v"
 
-`include "./rtl/alu/alu.v"
+`include "./rtl/alu.v"
 
 
 module cpu (
-	input reset,
-    input clock,
+	input wire reset,
+    input wire clock,
 
-    output reg [31:0] inst_aout,
-    input [31:0] inst_din,
+    input wire [31:0] inst_din,
+    input wire [31:0] mem_din,
 
-    output reg mem_rw,
+    output wire [31:0] inst_aout,
+
+    output wire mem_rw,
     output reg [31:0] mem_aout,
-    output reg [31:0] mem_dout,
-    input [31:0] mem_din
+    output reg [31:0] mem_dout
 );
 
     /* PC pipeline registers */
     reg [31:0] pc_pipe[3:0];
 
     /* IR pipeline registers */
-    reg [31:0] ir_pipe[3:0];
+    reg [31:0] ir_pipe[2:0];
 
     /* Data pipeline registers */
     reg [31:0] d_pipe[1:0];
-    reg [31:0] a_pipe;
-    reg [31:0] b_pipe;
     reg [31:0] r_pipe[1:0];
 
     /* Control signals */
-    reg [1:0] pc_sel;
-    reg pc_load;
+    wire [1:0] pc_sel;
 
-    reg [1:0] a_sel;
-    reg b_sel;
+    wire [1:0] a_sel;
+    wire b_sel;
 
-    reg [1:0] store_sel;
-    reg [2:0] load_sel;
-    reg [1:0] reg_sel;
-    reg reg_we;
+    wire [1:0] store_sel;
+    wire [2:0] load_sel;
+    wire [1:0] reg_sel;
+    wire reg_we;
 
-    reg [1:0] ir_sel;
-    reg branch_sel;
-    reg stall;
+    wire [1:0] ir_sel;
+    wire branch_sel;
+    wire stall;
     
     /* Other signals */
-    reg [31:0] branch_res;
-    reg [31:0] branch;
-    reg [31:0] jal;
-    reg [31:0] jalr;
+    wire [31:0] branch_res;
+    wire [31:0] branch;
 
     /* program counter */    
-    reg [31:0] pc;
+    wire [31:0] pc;
+    wire [31:0] alu_res;
 
     pc pc_module (
         .clock  ( clock ),
         .reset  ( reset ),
-        .load   ( pc_load ),
 
         .pc_sel ( pc_sel ),
-        .jal    ( jal ),
-        .jalr   ( jalr ),
+        .jal    ( alu_res ),
+        .jalr   ( alu_res ),
         .branch ( branch_res ),
 
         .pc     ( pc )
     );
      
     /* RR decoder */
-    reg [4:0] rs1;
-    reg [4:0] rs2;
-    reg [31:0] rr_imm;
+    wire [4:0] rs1;
+    wire [4:0] rs2;
+    wire [31:0] rr_imm;
 
     rr_decoder rr_decoder_module (
         .clock  ( clock ),
         
-        .inst   ( ir_pipe[0] ),
+        .inst   ( inst_din ),
 
         .rs1    ( rs1 ),
         .rs2    ( rs2 ),
@@ -96,13 +92,13 @@ module cpu (
     );
 
     /* ALU decoder */
-    reg [31:0] alu_imm;
-    reg [3:0] alu_op;
+    wire [31:0] alu_imm;
+    wire [3:0]  alu_op;
 
     alu_decoder alu_decoder_module (
         .clock  ( clock ),
 
-        .inst   ( ir_pipe[1] ),
+        .inst   ( ir_pipe[0] ),
 
         .imm    ( alu_imm ),
 
@@ -113,35 +109,37 @@ module cpu (
     mem_decoder mem_decoder_module (
         .clock     ( clock ),
 
-        .inst      ( ir_pipe[2] ),
+        .inst      ( ir_pipe[1] ),
 
         .rw        ( mem_rw ),
         .store_sel ( store_sel )
     );
 
     /* WB decoder */
+    wire [4:0] rd;
+
     wb_decoder wb_decoder_module (
         .clock    ( clock ),
 
-        .inst     ( ir_pipe[3] ),
+        .inst     ( ir_pipe[2] ),
 
+        .rd       ( rd ),
         .load_sel ( load_sel ),
         .reg_we   ( reg_we ),
         .reg_sel  ( reg_sel )
     );
 
     /* Instantiate register bank */
-    reg [4:0]  reg_ain;
-    reg [31:0] reg_din;
+    wire [31:0] reg_din;
 
-    reg [31:0] rs1_val;
-    reg [31:0] rs2_val;
+    wire [31:0] rs1_val;
+    wire [31:0] rs2_val;
 
     register_bank register_bank_module (
         .clock   ( clock ),
 
         .we      ( reg_we ),
-        .ain     ( reg_ain ),
+        .ain     ( rd ),
         .din     ( reg_din ),
         
         .rs1     ( rs1 ),
@@ -152,65 +150,39 @@ module cpu (
     );
 
     /* Instantiate ALU */
-    reg [31:0] a;
-    reg [31:0] b;
-    reg [31:0] alu_res;
-    reg cond;
+    wire [31:0] a;
+    wire [31:0] b;
 
     alu alu_module (
         .clock  ( clock ),
 
         .alu_op ( alu_op ),
-        .a      ( a_pipe ),
-        .b      ( b_pipe ),
+        .a      ( a ),
+        .b      ( b ),
 
         .res    ( alu_res ),
-        .cond   ( cond )
+        .cond   ( branch_sel )
     );
 
     assign branch = alu_imm + pc_pipe[1];
 
     /* a select mux */
-    always @(a_sel, pc_pipe[0], rs1_val) begin
-        case (a_sel)
-            `A_PC:   a = pc_pipe[0];
-            `A_REG:  a = rs1_val;
-            default  a = 0;
-        endcase
-    end
+    assign a = (a_sel == `A_PC)  ? pc_pipe[0] :
+               (a_sel == `A_REG) ? rs1_val : 0;
 
     /* b select mux */
-    always @(b_sel, rr_imm, rs2_val) begin
-        case (b_sel)
-            `B_IMM: b = rr_imm;
-            `B_REG: b = rs2_val;
-        endcase
-    end
+    assign b = (b_sel == `B_IMM) ? rr_imm : rs2_val;
 
-    /* b select mux */
-    always @(reg_sel, pc_pipe[3], mem_din, r_pipe[1]) begin
-        case (reg_sel)
-            `REG_PC_PLUS_4: reg_din = pc_pipe[3] + 4;
-            `REG_RES:       reg_din = r_pipe[1];
-            `REG_MEM:       reg_din = mem_din;
-            default:        reg_din = r_pipe[1];
-        endcase
-    end
+    /* register select mux */
+    assign reg_din = (reg_sel == `REG_PC_PLUS_4) ? pc_pipe[3] + 1 :
+                     (reg_sel == `REG_RES)       ? r_pipe[1] : mem_din;
 
     /* branch select mux */
-    always @(branch_sel, branch, pc) begin
-        case (branch_sel)
-            `YES_BRANCH: branch_res = branch;
-            `NO_BRANCH:  branch_res = pc + 4;
-        endcase
-    end
+    assign branch_res = (branch_sel == `YES_BRANCH) ? branch : pc + 1;
+
+    assign inst_aout = pc;
 
     always @(posedge clock) begin
-        /* Fetch instruction */
-        pc_load <= 1;
-        inst_aout  <= pc;
-        ir_pipe[0] <= inst_din; 
-
         /* Pipeline */
         pc_pipe[0] <= pc;
         pc_pipe[1] <= pc_pipe[0];
@@ -220,18 +192,15 @@ module cpu (
         ir_pipe[0] <= inst_din;
         ir_pipe[1] <= ir_pipe[0];
         ir_pipe[2] <= ir_pipe[1];
-        ir_pipe[3] <= ir_pipe[2];
 
         d_pipe[0]  <= rs2_val;
         d_pipe[1]  <= d_pipe[0];
-
-        a_pipe     <= a;
-        b_pipe     <= b;
+        mem_dout   <= d_pipe[1];
 
         r_pipe[0]  <= alu_res;
-        r_pipe[1]  <= r_pipe[0];  
+        r_pipe[1]  <= r_pipe[0]; 
+        mem_aout   <= r_pipe[0]; 
     end
-
 
 endmodule
 
