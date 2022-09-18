3c8a59727e753449b1bdf24f0d6616e1f6973230
Tom Finet <tom.codeninja@gmail.com>
2022-09-18 19:20:56 +0200

    stall logic and decoders now form ir_pipeline

--- rtl/cpu.v
+++ rtl/cpu.v
@@ -23,14 +23,14 @@ module cpu (
     output wire [31:0] inst_aout,
 
     output wire mem_rw,
-    output reg [31:0] mem_aout,
-    output reg [31:0] mem_dout
+    output wire [31:0] mem_aout,
+    output wire [31:0] mem_dout
 );
 
     /* PC pipeline registers */
     reg [31:0] pc_pipe[3:0];
 
-    /* IR pipeline registers */
+    /* IR pipeline */
     reg [31:0] ir_pipe[2:0];
 
     /* Data pipeline registers */
@@ -48,9 +48,11 @@ module cpu (
     wire [1:0] reg_sel;
     wire reg_we;
 
-    wire [1:0] ir_sel;
+    wire ir_sel;
     wire branch_sel;
     wire stall;
+    wire data_stall;
+    wire control_stall;
     
     /* Other signals */
     wire [31:0] branch_res;
@@ -58,10 +60,11 @@ module cpu (
 
     /* program counter */    
     wire [31:0] pc;
+    wire        pc_on;
     wire [31:0] alu_res;
 
     pc pc_module (
-        .clock  ( clock ),
+        .clock  ( pc_on ),
         .reset  ( reset ),
 
         .pc_sel ( pc_sel ),
@@ -73,57 +76,71 @@ module cpu (
     );
      
     /* RR decoder */
-    wire [4:0] rs1;
-    wire [4:0] rs2;
+    wire [4:0]  rs1;
+    wire [4:0]  rs2;
     wire [31:0] rr_imm;
+    wire [4:0]  rr_rd;
+    wire [31:0] rr_inst;
+    wire rr_control_stall;
 
     rr_decoder rr_decoder_module (
-        .clock  ( clock ),
+        .clock    ( clock ),
         
-        .inst   ( inst_din ),
+        .inst     ( rr_inst ),
+        .inst_out ( ir_pipe[0] ),
 
-        .rs1    ( rs1 ),
-        .rs2    ( rs2 ),
-        .imm    ( rr_imm ),
+        .rs1      ( rs1 ),
+        .rs2      ( rs2 ),
+        .imm      ( rr_imm ),
 
-        .pc_sel ( pc_sel ),
-        .a_sel  ( a_sel ),
-        .b_sel  ( b_sel )
+        .pc_sel   ( pc_sel ),
+        .a_sel    ( a_sel ),
+        .b_sel    ( b_sel ),
+        .rd       ( rr_rd ),
+        .stall    ( rr_control_stall )
     );
 
     /* ALU decoder */
     wire [31:0] alu_imm;
     wire [3:0]  alu_op;
+    wire [4:0]  alu_rd;
+    wire alu_control_stall;
 
     alu_decoder alu_decoder_module (
-        .clock  ( clock ),
+        .clock    ( clock ),
 
-        .inst   ( ir_pipe[0] ),
+        .inst     ( ir_pipe[0] ),
+        .inst_out ( ir_pipe[1] ),
 
-        .imm    ( alu_imm ),
-
-        .alu_op ( alu_op )
+        .alu_op   ( alu_op ),
+        
+        .imm      ( alu_imm ),
+        .rd       ( alu_rd ),
+        .stall    ( alu_control_stall )
     );
 
     /* MEM decoder */
+    wire [4:0] mem_rd;
     mem_decoder mem_decoder_module (
         .clock     ( clock ),
 
         .inst      ( ir_pipe[1] ),
+        .inst_out  ( ir_pipe[2] ),
 
         .rw        ( mem_rw ),
-        .store_sel ( store_sel )
+        .store_sel ( store_sel ),
+        .rd        ( mem_rd )
     );
 
     /* WB decoder */
-    wire [4:0] rd;
+    wire [4:0] wb_rd;
 
     wb_decoder wb_decoder_module (
         .clock    ( clock ),
 
         .inst     ( ir_pipe[2] ),
 
-        .rd       ( rd ),
+        .rd       ( wb_rd ),
         .load_sel ( load_sel ),
         .reg_we   ( reg_we ),
         .reg_sel  ( reg_sel )
@@ -139,7 +156,7 @@ module cpu (
         .clock   ( clock ),
 
         .we      ( reg_we ),
-        .ain     ( rd ),
+        .ain     ( wb_rd ),
         .din     ( reg_din ),
         
         .rs1     ( rs1 ),
@@ -182,6 +199,35 @@ module cpu (
 
     assign inst_aout = pc;
 
+    assign control_stall = rr_control_stall || alu_control_stall;
+
+    assign data_stall =
+                (
+                    inst_din[6:0] == `LUI ||
+                    inst_din[6:0] == `AUIPC ||
+                    inst_din[6:0] == `JAL
+                ) ? 0 :
+                (
+                    inst_din[19:15] != `ZERO_REG && (
+                    inst_din[19:15] == rr_rd ||
+                    inst_din[19:15] == alu_rd ||
+                    inst_din[19:15] == mem_rd)
+                ) || (
+                    inst_din[24:20] != `ZERO_REG && (
+                    inst_din[24:20] == rr_rd ||
+                    inst_din[24:20] == alu_rd ||
+                    inst_din[24:20] == mem_rd)
+                ) ? 1 : 0;
+
+    assign stall = data_stall || control_stall;
+
+    assign pc_on = stall ? 0 : clock;
+
+    assign rr_inst = stall ? `NOP : inst_din;
+
+    assign mem_aout = alu_res;
+    assign mem_dout = d_pipe[1];
+
     always @(posedge clock) begin
         /* Pipeline */
         pc_pipe[0] <= pc;
@@ -189,17 +235,11 @@ module cpu (
         pc_pipe[2] <= pc_pipe[1];
         pc_pipe[3] <= pc_pipe[2];
 
-        ir_pipe[0] <= inst_din;
-        ir_pipe[1] <= ir_pipe[0];
-        ir_pipe[2] <= ir_pipe[1];
-
         d_pipe[0]  <= rs2_val;
         d_pipe[1]  <= d_pipe[0];
-        mem_dout   <= d_pipe[1];
 
         r_pipe[0]  <= alu_res;
-        r_pipe[1]  <= r_pipe[0]; 
-        mem_aout   <= r_pipe[0]; 
+        r_pipe[1]  <= r_pipe[0];
     end
 
 endmodule
