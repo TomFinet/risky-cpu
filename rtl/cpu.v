9ee9a367f2041c653d4e90f19d1114918901cf88
Tom Finet <tom.codeninja@gmail.com>
2022-09-19 20:46:19 +0200

    added resets to all clocked modules

--- rtl/cpu.v
+++ rtl/cpu.v
@@ -2,15 +2,13 @@
 `define CPU
 
 `include "./rtl/codes.v"
-`include "./rtl/pc.v"
 `include "./rtl/register_bank.v"
-
-`include "./rtl/rr_decoder.v"
-`include "./rtl/alu_decoder.v"
-`include "./rtl/mem_decoder.v"
-`include "./rtl/wb_decoder.v"
-
+`include "./rtl/rr_control.v"
+`include "./rtl/alu_control.v"
+`include "./rtl/mem_control.v"
+`include "./rtl/wb_control.v"
 `include "./rtl/alu.v"
+`include "./rtl/stall_unit.v"
 
 
 module cpu (
@@ -28,17 +26,23 @@ module cpu (
 );
 
     /* PC pipeline registers */
+    reg [31:0] pc;
     reg [31:0] pc_pipe[3:0];
 
     /* IR pipeline */
-    reg [31:0] ir_pipe[2:0];
+    wire [31:0] ir_pipe[2:0];
 
     /* Data pipeline registers */
+    reg [31:0] a_pipe;
+    reg [31:0] b_pipe;
+
     reg [31:0] d_pipe[1:0];
     reg [31:0] r_pipe[1:0];
 
     /* Control signals */
     wire [1:0] pc_sel;
+    wire [1:0] rr_pc_sel;
+    wire [1:0] alu_pc_sel;
 
     wire [1:0] a_sel;
     wire b_sel;
@@ -48,7 +52,6 @@ module cpu (
     wire [1:0] reg_sel;
     wire reg_we;
 
-    wire ir_sel;
     wire branch_sel;
     wire stall;
     wire data_stall;
@@ -58,33 +61,39 @@ module cpu (
     wire [31:0] branch_res;
     wire [31:0] branch;
 
-    /* program counter */    
-    wire [31:0] pc;
+    wire [31:0] a;
+    wire [31:0] b;
+
     wire        pc_on;
     wire [31:0] alu_res;
 
-    pc pc_module (
-        .clock  ( pc_on ),
-        .reset  ( reset ),
-
-        .pc_sel ( pc_sel ),
-        .jal    ( alu_res ),
-        .jalr   ( alu_res ),
-        .branch ( branch_res ),
-
-        .pc     ( pc )
-    );
-     
-    /* RR decoder */
     wire [4:0]  rs1;
     wire [4:0]  rs2;
     wire [31:0] rr_imm;
-    wire [4:0]  rr_rd;
     wire [31:0] rr_inst;
-    wire rr_control_stall;
+    wire [4:0]  rr_rd;
+    wire [6:0]  rr_opcode;
 
-    rr_decoder rr_decoder_module (
+    wire [31:0] alu_imm;
+    wire [3:0]  alu_op;
+    wire [4:0]  alu_rd;
+    wire [6:0]  alu_opcode;
+
+    wire [4:0] mem_rd;
+    wire [6:0] mem_opcode;
+
+    wire [4:0] wb_rd;
+
+    wire [31:0] reg_din;
+
+    wire [31:0] rs1_val;
+    wire [31:0] rs2_val;
+
+
+    /* RR control */
+    rr_control rr_control_m (
         .clock    ( clock ),
+        .reset    ( reset ),
         
         .inst     ( rr_inst ),
         .inst_out ( ir_pipe[0] ),
@@ -93,53 +102,50 @@ module cpu (
         .rs2      ( rs2 ),
         .imm      ( rr_imm ),
 
-        .pc_sel   ( pc_sel ),
+        .pc_sel   ( rr_pc_sel ),
         .a_sel    ( a_sel ),
         .b_sel    ( b_sel ),
         .rd       ( rr_rd ),
-        .stall    ( rr_control_stall )
+        .opcode   ( rr_opcode )
     );
 
-    /* ALU decoder */
-    wire [31:0] alu_imm;
-    wire [3:0]  alu_op;
-    wire [4:0]  alu_rd;
-    wire alu_control_stall;
-
-    alu_decoder alu_decoder_module (
+    /* ALU control */
+    alu_control alu_control_m (
         .clock    ( clock ),
+        .reset    ( reset ),
 
         .inst     ( ir_pipe[0] ),
         .inst_out ( ir_pipe[1] ),
 
         .alu_op   ( alu_op ),
-        
+
+        .pc_sel   ( alu_pc_sel ),
         .imm      ( alu_imm ),
         .rd       ( alu_rd ),
-        .stall    ( alu_control_stall )
+        .opcode   ( alu_opcode )
     );
 
     /* MEM decoder */
-    wire [4:0] mem_rd;
-    mem_decoder mem_decoder_module (
+    mem_control mem_control_m (
         .clock     ( clock ),
+        .reset     ( reset ),
 
         .inst      ( ir_pipe[1] ),
         .inst_out  ( ir_pipe[2] ),
 
         .rw        ( mem_rw ),
         .store_sel ( store_sel ),
-        .rd        ( mem_rd )
+        .rd        ( mem_rd ),
+        .opcode    ( mem_opcode )
     );
 
     /* WB decoder */
-    wire [4:0] wb_rd;
-
-    wb_decoder wb_decoder_module (
+    wb_control wb_control_m (
         .clock    ( clock ),
+        .reset    ( reset ),
 
         .inst     ( ir_pipe[2] ),
-
+        
         .rd       ( wb_rd ),
         .load_sel ( load_sel ),
         .reg_we   ( reg_we ),
@@ -147,13 +153,9 @@ module cpu (
     );
 
     /* Instantiate register bank */
-    wire [31:0] reg_din;
-
-    wire [31:0] rs1_val;
-    wire [31:0] rs2_val;
-
-    register_bank register_bank_module (
+    register_bank register_bank_m (
         .clock   ( clock ),
+        .reset   ( reset ),
 
         .we      ( reg_we ),
         .ain     ( wb_rd ),
@@ -167,21 +169,36 @@ module cpu (
     );
 
     /* Instantiate ALU */
-    wire [31:0] a;
-    wire [31:0] b;
-
-    alu alu_module (
-        .clock  ( clock ),
-
+    alu alu_m (
         .alu_op ( alu_op ),
-        .a      ( a ),
-        .b      ( b ),
+        .a      ( a_pipe ),
+        .b      ( b_pipe ),
 
         .res    ( alu_res ),
         .cond   ( branch_sel )
     );
 
-    assign branch = alu_imm + pc_pipe[1];
+    /* Pipeline hazard logic */
+    stall_unit stall_unit_m (
+        .if_rs1        ( inst_din[19:15] ),
+        .if_rs2        ( inst_din[24:20] ),
+        .if_opcode     ( inst_din[6:0] ),
+
+        .rr_opcode     ( rr_opcode ),
+        .alu_opcode    ( alu_opcode ),
+        .mem_opcode    ( mem_opcode ),
+
+        .rr_rd         ( rr_rd ),
+        .alu_rd        ( alu_rd ),
+        .mem_rd        ( mem_rd ),
+
+        .data_stall    ( data_stall ),
+        .control_stall ( control_stall )
+    );
+
+    assign stall = data_stall || control_stall;
+
+    assign pc_on = stall ? 0 : clock;
 
     /* a select mux */
     assign a = (a_sel == `A_PC)  ? pc_pipe[0] :
@@ -195,51 +212,49 @@ module cpu (
                      (reg_sel == `REG_RES)       ? r_pipe[1] : mem_din;
 
     /* branch select mux */
+    assign branch = alu_imm + pc_pipe[1];
     assign branch_res = (branch_sel == `YES_BRANCH) ? branch : pc + 1;
-
-    assign inst_aout = pc;
-
-    assign control_stall = rr_control_stall || alu_control_stall;
-
-    assign data_stall =
-                (
-                    inst_din[6:0] == `LUI ||
-                    inst_din[6:0] == `AUIPC ||
-                    inst_din[6:0] == `JAL
-                ) ? 0 :
-                (
-                    inst_din[19:15] != `ZERO_REG && (
-                    inst_din[19:15] == rr_rd ||
-                    inst_din[19:15] == alu_rd ||
-                    inst_din[19:15] == mem_rd)
-                ) || (
-                    inst_din[24:20] != `ZERO_REG && (
-                    inst_din[24:20] == rr_rd ||
-                    inst_din[24:20] == alu_rd ||
-                    inst_din[24:20] == mem_rd)
-                ) ? 1 : 0;
-
-    assign stall = data_stall || control_stall;
-
-    assign pc_on = stall ? 0 : clock;
 
     assign rr_inst = stall ? `NOP : inst_din;
 
-    assign mem_aout = alu_res;
+    assign inst_aout = pc;
+    assign mem_aout = r_pipe[0];
     assign mem_dout = d_pipe[1];
 
+    assign pc_sel = (
+                        alu_pc_sel == `PC_JUMP ||
+                        alu_pc_sel == `PC_BRANCH
+                    ) ? alu_pc_sel : rr_pc_sel;
+
     always @(posedge clock) begin
-        /* Pipeline */
-        pc_pipe[0] <= pc;
-        pc_pipe[1] <= pc_pipe[0];
-        pc_pipe[2] <= pc_pipe[1];
-        pc_pipe[3] <= pc_pipe[2];
+        if (reset) begin
+            pc <= 0;
+        end
+        else begin
+            if (!stall) begin
+                case (pc_sel)
+                    `PC_PLUS_1     : pc <= pc + 1;
+                    `PC_BRANCH     : pc <= branch;
+                    `PC_JUMP       : pc <= alu_res;
+                    default        : pc <= pc + 1;
+                endcase
+            end
 
-        d_pipe[0]  <= rs2_val;
-        d_pipe[1]  <= d_pipe[0];
+            /* Pipeline */
+            pc_pipe[0] <= pc;
+            pc_pipe[1] <= pc_pipe[0];
+            pc_pipe[2] <= pc_pipe[1];
+            pc_pipe[3] <= pc_pipe[2];
 
-        r_pipe[0]  <= alu_res;
-        r_pipe[1]  <= r_pipe[0];
+            a_pipe     <= a;
+            b_pipe     <= b; 
+
+            d_pipe[0]  <= rs2_val;
+            d_pipe[1]  <= d_pipe[0];
+
+            r_pipe[0]  <= alu_res;
+            r_pipe[1]  <= r_pipe[0];
+        end
     end
 
 endmodule
