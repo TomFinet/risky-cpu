2c0b8d78743e1fed98372cb75f49776428187aec
Tom Finet <tom.codeninja@gmail.com>
2022-09-19 20:47:21 +0200

    added reset signal

--- rtl/register_bank.v
+++ rtl/register_bank.v
@@ -6,6 +6,7 @@
 
 module register_bank(
 	input wire clock,
+    input wire reset,
 
 	input wire        we,
 	input wire [4:0]  ain,
@@ -33,9 +34,45 @@ module register_bank(
     assign rs2_val = r[rs2];
 
 	always @(posedge clock) begin
-		r[0] <= 0;
-        if(we && ain != 5'b00000) begin
-			r[ain] <= din;
+        if (reset) begin
+            r[0]  <= 0;
+            r[1]  <= 0;
+            r[2]  <= 0;
+            r[3]  <= 0;
+            r[4]  <= 0;
+            r[5]  <= 0;
+            r[6]  <= 0;
+            r[7]  <= 0;
+            r[8]  <= 0;
+            r[9]  <= 0;
+            r[10] <= 0;
+            r[11] <= 0;
+            r[12] <= 0;
+            r[13] <= 0;
+            r[14] <= 0;
+            r[15] <= 0;
+            r[16] <= 0;
+            r[17] <= 0;
+            r[18] <= 0;
+            r[19] <= 0;
+            r[20] <= 0;
+            r[21] <= 0;
+            r[22] <= 0;
+            r[23] <= 0;
+            r[24] <= 0;
+            r[25] <= 0;
+            r[26] <= 0;
+            r[27] <= 0;
+            r[28] <= 0;
+            r[29] <= 0;
+            r[30] <= 0;
+            r[31] <= 0;
+        end
+        else begin
+            r[0] <= 0;
+            if(we && ain != 5'b00000) begin
+                r[ain] <= din;
+            end
         end
 	end
 
